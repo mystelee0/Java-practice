92635309b6c2e649c73102390775b6c4e450888b
mystelee0 <99077666+mystelee0@users.noreply.github.com>
2022-02-11 15:36:48 +0900

    7-1연습문제 섯다카드

---
+++ Exercise7.java
@@ -0,0 +1,42 @@
+class SutdaDeck{
+	final int CARD_NUM=20;
+	SutdaCard[] cards = new SutdaCard[CARD_NUM];
+
+	SutdaDeck()
+	{
+		for(int i=0;i<20;i++)
+		{
+			if(i<10&&(i==0||i==2||i==7))
+				cards[i]=new SutdaCard(i%10+1,true);
+			
+			else
+				cards[i]=new SutdaCard(i%10+1,false);
+		
+		}
+	}
+}
+
+class SutdaCard{
+	int num;
+	boolean isKwang;
+
+	SutdaCard(){
+		this(1,true);
+	}
+	SutdaCard(int num,boolean isKwang){
+		this.num=num;
+		this.isKwang=isKwang;
+	}
+	
+	public String toString(){
+		return num+(isKwang ? "K":"");
+	}
+}
+
+class Exercise7{
+	public static void main(String[] args){
+		SutdaDeck deck =new SutdaDeck();
+		for(int i=0;i<deck.cards.length;i++)
+			System.out.println(deck.cards[i]+",");
+	}
+}
